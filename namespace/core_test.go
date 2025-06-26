496867436aa9930d241d257433ba21af5fba472b
poteto0 <mahiro.fukuda.aries@gmail.com>
2025-06-26 21:47:33 +0900

    feat: core.GetGasPrice. refs: #17

--- namespace/core_test.go
+++ namespace/core_test.go
@@ -89,3 +89,61 @@ func TestCore_GetBlockNumber(t *testing.T) {
 		})
 	})
 }
+
+func TestCore_GetGasPrice(t *testing.T) {
+	// Arrange
+	provider := newProvider()
+	core := namespace.NewCore(provider).(*namespace.Core)
+
+	t.Run("normal case:", func(t *testing.T) {
+		t.Run("return block number", func(t *testing.T) {
+			patches := gomonkey.NewPatches()
+			defer patches.Reset()
+
+			// Arrange
+			expectedNumber := 100
+
+			// Mock
+			patches.ApplyMethod(
+				reflect.TypeOf(provider),
+				"GetGasPrice",
+				func(_ *alchemy.AlchemyProvider) (int, error) {
+					return expectedNumber, nil
+				},
+			)
+
+			// Act
+			price, err := core.GetGasPrice()
+
+			// Assert
+			assert.NoError(t, err)
+			assert.Equal(t, expectedNumber, price)
+		})
+	})
+
+	t.Run("error case:", func(t *testing.T) {
+		t.Run("return 0 & provider error", func(t *testing.T) {
+			patches := gomonkey.NewPatches()
+			defer patches.Reset()
+
+			// Arrange
+			errExpected := errors.New("error")
+
+			// Mock
+			patches.ApplyMethod(
+				reflect.TypeOf(provider),
+				"GetGasPrice",
+				func(_ *alchemy.AlchemyProvider) (int, error) {
+					return 0, errExpected
+				},
+			)
+
+			// Act
+			price, err := core.GetGasPrice()
+
+			// Assert
+			assert.ErrorIs(t, errExpected, err)
+			assert.Equal(t, 0, price)
+		})
+	})
+}
