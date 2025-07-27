ad582f9d99057a1d7044688fbcbcf1b08a24bfb2
poteto0 <mahiro.fukuda.aries@gmail.com>
2025-07-27 12:29:14 +0900

    ref: refactor create body to bytes. refs: #44

--- utils/body.go
+++ utils/body.go
@@ -7,20 +7,22 @@ import (
 	"github.com/poteto-go/go-alchemy-sdk/types"
 )
 
-func createRequestBody[T string | types.TransactionRequest | types.Filter](id int, method string, params []T) types.AlchemyRequestBody[T] {
+/*
+1. parse params to byte array
+2. parse request body to byte array
+*/
+func CreateRequestBodyToBytes[
+	T string | types.TransactionRequest | types.Filter,
+](id int, method string, params []T) ([]byte, error) {
 	body := types.AlchemyRequestBody[T]{
 		Id:      id,
 		Jsonrpc: "2.0",
 		Method:  method,
 	}
+
 	if len(params) > 0 {
 		body.Params = params
 	}
-	return body
-}
-
-func CreateRequestBodyToBytes[T string | types.TransactionRequest | types.Filter](id int, method string, params []T) ([]byte, error) {
-	body := createRequestBody(id, method, params)
 
 	jsonBody, err := json.Marshal(body)
 	if err != nil {
