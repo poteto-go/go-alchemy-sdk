5edc3cc4cdb63b665709ef8cc892e8829f1be400
poteto0 <mahiro.fukuda.aries@gmail.com>
2025-12-27 00:05:22 +0900

    :rocket: add factory for type safe call. refs: #245

---
+++ factory/wallet_factory.go
@@ -0,0 +1,34 @@
+package factory
+
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/v2"
+	"github.com/poteto-go/go-alchemy-sdk/types"
+	"github.com/poteto-go/go-alchemy-sdk/wallet"
+)
+
+func ContractCall[T any](
+	w wallet.Wallet,
+	contract types.ContractInstance,
+	contractAddress string,
+	opts *bind.CallOpts,
+	callData []byte,
+	unpack func([]byte) (T, error),
+) (T, error) {
+	unpackAny := func(b []byte) (any, error) {
+		return unpack(b)
+	}
+
+	res, err := w.ContractCall(contract, contractAddress, opts, callData, unpackAny)
+	if err != nil {
+		return *new(T), err
+	}
+
+	val, ok := res.(T)
+	if !ok {
+		return *new(T), fmt.Errorf("failed to cast result to expected type")
+	}
+
+	return val, nil
+}
