f40bd52c43a3726069d776b2d0f52ec3d27f8f36
poteto <67512297+poteto0@users.noreply.github.com>
2025-12-31 17:45:49 +0900

    :rocket: p8 network selector. refs: #238 (#247)
    * :rocket: p8 network selector. refs: #238
    
    * :pencil2: fix typo. refs: #238

--- gas/alchemy_config_test.go
+++ gas/alchemy_config_test.go
@@ -38,6 +38,32 @@ func TestNewAlchemyConfig(t *testing.T) {
 		// Assert
 		assert.Equal(t, config.url, "http://127.0.0.1:8080")
 	})
+
+	t.Run("use private network func", func(t *testing.T) {
+		// Arrange
+		env := "prod"
+		networkSelector := func(setting AlchemySetting) bool {
+			return env == "develop"
+		}
+
+		// Act
+		config := NewAlchemyConfig(
+			AlchemySetting{
+				ApiKey:  "api-key",
+				Network: types.MaticMainnet,
+				PrivateNetworkConfig: PrivateNetworkConfig{
+					Host: "127.0.0.1",
+					Port: 8080,
+				},
+				IsPrivateNetwork: networkSelector,
+			},
+		)
+
+		// Assert
+		assert.Equal(t, config.apiKey, "api-key")
+		assert.Equal(t, string(config.network), "matic-mainnet")
+		assert.Equal(t, config.url, "https://matic-mainnet.g.alchemy.com/v2/api-key")
+	})
 }
 
 func TestAlchemyConfig_GetUrl(t *testing.T) {
